a07c93e9079f1412c4213f50ab21e1de8579b652
Simplicity is key <shubhambanne1819@gmail.com>
2023-01-15 09:34:01 -0800

    fixed inventory api

--- inventory/inventory.py
+++ inventory/inventory.py
@@ -3,3 +3,7 @@ from flask import Blueprint, render_template, request, session, redirect
 inventory_blueprint = Blueprint(
     "inventory", __name__, template_folder="templates", static_folder="static"
 )
+
+@inventory_blueprint.route("/inventory")
+def render_inventory():
+    return render_template("inventory.html")
