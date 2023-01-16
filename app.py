f65d87292e91e3e09fcdfb45597a38f802898138
Simplicity is key <shubhambanne1819@gmail.com>
2023-01-16 07:58:06 -0800

    database connection

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template
 from inventory.inventory import inventory_blueprint
+from connector import mongo_conn
 
 app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/tractor_dealer?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
+mongo_conn.init_app(app)
+
 
 # registering blueprints
 app.register_blueprint(inventory_blueprint)
@@ -10,6 +14,7 @@ app.register_blueprint(inventory_blueprint)
 @app.route("/")
 def index():
     return render_template("home.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- inventory/inventory.py
+++ inventory/inventory.py
@@ -1,11 +1,14 @@
 from flask import Blueprint, render_template, request, session, redirect
+from connector import mongo_conn
 
 inventory_blueprint = Blueprint(
     "inventory", __name__, template_folder="templates", static_folder="static"
 )
 
+
 @inventory_blueprint.route("/inventory", methods=["get", "post"])
 def render_inventory():
-    print(request.method)
-    print(dict(request.form))
+    tractor_details = dict(request.form)
+    print(tractor_details)
+    mongo_conn.db.stock_tractor.insert_one(tractor_details)
     return render_template("inventory.html")
