260a12b09a5f941fc73e4a420e502bbd90a40124
RavirajPawar <pawarraviraj9160@gmail.com>
2023-02-08 22:29:27 +0530

    Sell tractor table and form

--- orders/orders.py
+++ orders/orders.py
@@ -10,6 +10,27 @@ orders_blueprint = Blueprint(
 )
 
 
-@orders_blueprint.route("/sell-tractor", methods=["GET", "POST"])
-def sell_tractor():
-    return render_template("sell_tractor.html")
+@orders_blueprint.route("/sell-tractor", methods=["GET"])
+@orders_blueprint.route("/sell-tractor/<string:tractor>/", methods=["GET"])
+def sell_tractor(tractor=None, display_tractor=dict()):
+    if tractor:
+        result = mongo_conn.db.stock_tractor.find_one({"chassis-number":tractor}, {"_id": 0})
+        return render_template("cart_tractor.html", tractor=tractor, result=result)
+
+    if not tractor:
+        logger.info("started processing sell-tractor".center(80, "^"))
+        result = mongo_conn.db.stock_tractor.find({}, {"_id": 0})
+        all_tractor = list()
+        for item in result:
+            all_tractor.append(dict(item))
+            if not tractor:
+                display_tractor = all_tractor[-1]
+                tractor = True
+            elif item["chassis-number"] == tractor:
+                display_tractor = all_tractor[-1]
+        logger.info("Finished processing sell-tractor".center(80, "^"))
+        return render_template(
+            "sell_tractor.html",
+            all_tractor=all_tractor,
+            display_tractor=display_tractor,
+        )
